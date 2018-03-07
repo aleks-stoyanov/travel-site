acdde490dfedbd1079eb7d21badd4b539cf1619a
Aleks <aleks.stoyanov@gmail.com>
2018-03-07 22:33:56 +0200

    Completed the modal overlay

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,45 @@
+import $ from 'jquery';
+
+class Modal {
+    constructor() {
+        this.openModalButton = $(".open-modal");
+        this.modal = $(".modal");
+        this.closeModalButton = $(".modal__close");
+        this.events();
+    }
+
+    events() {
+        //clicking the open modal Button
+        this.openModalButton.click(this.openModal.bind(this));
+
+
+        // clicking the x close modal Button
+        this.closeModalButton.click(this.closeModal.bind(this));
+
+
+        // pushes any key on Keyboard
+        $(document).keyup(this.keyPressHandler.bind(this));
+
+        
+    }
+
+    openModal(){
+        this.modal.addClass("modal--is-visible");
+        return false;
+    }
+
+
+    closeModal() {
+        this.modal.removeClass("modal--is-visible");
+    }
+
+    keyPressHandler (e) {
+        if (e.keyCode == 27) {
+            this.closeModal();
+        }
+    }
+    
+
+}
+
+export default Modal;
